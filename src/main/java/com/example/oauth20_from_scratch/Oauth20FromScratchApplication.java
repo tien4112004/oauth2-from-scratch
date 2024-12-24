b5f4929ffcd3b3e0aaf9b69bfab6d69ef1f7c356
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-12-24 07:31:37 +0700

    Initial commit

---
+++ src/main/java/com/example/oauth20_from_scratch/Oauth20FromScratchApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth20_from_scratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth20FromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth20FromScratchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oauth20_from_scratch/Oauth20FromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth20_from_scratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth20FromScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
